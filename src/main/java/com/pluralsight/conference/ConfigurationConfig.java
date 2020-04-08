0d6decbd4ea38f184a1407ae5c77dd02c6f6b2fe
santi <santicollazosbarrera@hotmail.com>
2020-04-07 20:42:51 -0500

    Implemented WebMvcConfigure to overwrite addResourceHandlers to add static files in the project

--- src/main/java/com/pluralsight/conference/ConfigurationConfig.java
+++ src/main/java/com/pluralsight/conference/ConfigurationConfig.java
@@ -3,10 +3,18 @@ package com.pluralsight.conference;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-public class ConfigurationConfig {
+public class ConfigurationConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("/pdf/");
+    }
 
     @Bean
     public ViewResolver viewResolver(){
